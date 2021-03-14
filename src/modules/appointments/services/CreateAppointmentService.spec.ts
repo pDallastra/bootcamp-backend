ca213422bca49c11e1650085bdcef52222066a8f
pDallastra <prpdallastra@gmail.com>
2021-03-14 16:54:29 -0300

    updateUserSerivice and TDD. Cleaning extra files.

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,15 +1,21 @@
 import AppError from '@shared/errors/AppError';
 import 'reflect-metadata';
 
-
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
-describe('CreateAppointment', () => {
-    it('should be able to create a new appointment', async () => {
-        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-        const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let createAppointment: CreateAppointmentService;
 
+describe('CreateAppointment', () => {
+    beforeEach(() => {
+        fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        createAppointment = new CreateAppointmentService(
+          fakeAppointmentsRepository,
+        );
+      });
+
+    it('should be able to create a new appointment', async () => {        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
         const appointmnet = await createAppointment.execute({
             date: new Date(),
             provider_id: '123123123123',
@@ -19,10 +25,7 @@ describe('CreateAppointment', () => {
         expect(appointmnet.provider_id).toBe('123123123123');
     })
 
-    it('should not be able to create an appointment on the same time', async () => {
-        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-        const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
-
+    it('should not be able to create an appointment on the same time', async () => {        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
         const appointmentDate = new Date(2021, 4, 10, 11);
         await createAppointment.execute({
             date: appointmentDate,
--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -1,17 +1,25 @@
 import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
 import AppError from '@shared/errors/AppError';
 
-
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import UpdateUserAvatarService from './UpdateUserAvatarService';
 
-describe('UpdateUserAvatar', () => {
-  it('should be able to update an user avatar', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeStorageProvider = new FakeStorageProvider();
-    
-    const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
+let fakeUsersRepository: FakeUsersRepository;
+let fakeStorageProvider: FakeStorageProvider;
+let updateUserAvatar: UpdateUserAvatarService;
 
+describe('UpdateUserAvatar', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeStorageProvider = new FakeStorageProvider();
+
+    updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+  });
+
+  it('should be able to update an user avatar', async () => {
     const user = await fakeUsersRepository.create({
         name: 'John Doe',
         email: 'johndoe@example.com',
@@ -27,11 +35,6 @@ describe('UpdateUserAvatar', () => {
   });
 
   it('should not be able to update avatar if user does not exists', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeStorageProvider = new FakeStorageProvider();
-    
-    const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
-
     await expect(updateUserAvatar.execute({
         user_id: 'non-existing-user',
         avatarFilename: 'avatar.jpg'
@@ -39,9 +42,6 @@ describe('UpdateUserAvatar', () => {
   });
 
   it('should delete avatar before adding new avatar', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeStorageProvider = new FakeStorageProvider();
-
     const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
     
     const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
